edfe66437ee134a953a0f77fa2f95d92da7c6606
Leah <leah.grace555@gmail.com>
2020-09-17 08:50:16 -0400

    started part two

---
+++ src/test/java/basic/library/TallyElectionTest.java
@@ -0,0 +1,9 @@
+package basic.library;
+
+import org.junit.Test;
+
+public class TallyElectionTest {
+    @Test public void testTallyElection () {
+        TallyElection tallyTest = new TallyElection();
+    }
+}
